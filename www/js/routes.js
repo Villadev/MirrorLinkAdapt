242fcc94f042220200189b4e67e544c1a2707504
AWS2-25 <AWS2-25>
2017-11-17 19:12:35 +0100

    Importacio ionic creator

---
+++ www/js/routes.js
@@ -0,0 +1,71 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('menu.home', {
+    url: '/page1',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('menu.map', {
+    url: '/map',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/map.html',
+        controller: 'mapCtrl'
+      }
+    }
+  })
+
+  .state('menu.configuraciN', {
+    url: '/config',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/configuraciN.html',
+        controller: 'configuraciNCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('menu.arxius', {
+    url: '/files',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/arxius.html',
+        controller: 'arxiusCtrl'
+      }
+    }
+  })
+
+  .state('menu.whatsapp', {
+    url: '/whatsapp',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/whatsapp.html',
+        controller: 'whatsappCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/page1')
+
+
+});
